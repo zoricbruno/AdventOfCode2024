a03b6d297f3a6c82f276bb5337b686a93d33b1df
Bruno <zoric.bruno@gmail.com>
2024-12-05 07:54:06 +0100

    Started Day03.

---
+++ Day03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText("Input/Input_0a.txt");
+Console.WriteLine(input);
+
+var mulPattern = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)");
+Match match = mulPattern.Match(input);
+Console.WriteLine(match);
+while (match.Success)
+{
+    match = match.NextMatch();
+    Console.WriteLine(match);
+    match.Value.
+}
