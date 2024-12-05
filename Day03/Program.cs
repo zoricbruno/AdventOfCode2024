8af83f63d5d897fe7cc05c24bd8d95ffcf97e822
Bruno <zoric.bruno@gmail.com>
2024-12-05 10:36:16 +0100

    Completed day 03 of AoC24

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,14 +1,66 @@
 ﻿using System.Text.RegularExpressions;
 
+// Task 1:
 var input = File.ReadAllText("Input/Input_0a.txt");
-Console.WriteLine(input);
+Console.WriteLine(MemoryUnscrambler.Multiply(input));
+input = File.ReadAllText("Input/Input_01.txt");
+Console.WriteLine(MemoryUnscrambler.Multiply(input));
 
-var mulPattern = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)");
-Match match = mulPattern.Match(input);
-Console.WriteLine(match);
-while (match.Success)
+// Task 2:
+input = File.ReadAllText("Input/Input_0b.txt");
+Console.WriteLine(MemoryUnscrambler.MultiplyWithConditions(input));
+input = File.ReadAllText("Input/Input_01.txt");
+Console.WriteLine(MemoryUnscrambler.MultiplyWithConditions(input));
+
+class MemoryUnscrambler
 {
-    match = match.NextMatch();
-    Console.WriteLine(match);
-    match.Value.
+    public static int MultiplyWithConditions(string input)
+    {
+        Regex mulPattern = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)");
+        Match match = mulPattern.Match(input);
+        int total = 0;
+        bool shouldMultiply = true;
+        while (match.Success)
+        {
+            if (match.Value == "do()")
+            {
+
+                shouldMultiply = true;
+            }
+            else if (match.Value == "don't()")
+            {
+                shouldMultiply = false;
+            }
+            else
+            {
+                var parts = match.Value.Split(
+                    new char[] { 'm', 'u', 'l', ',', '(', ')' },
+                    StringSplitOptions.RemoveEmptyEntries
+                );
+                if (shouldMultiply)
+                    total += int.Parse(parts[0]) * int.Parse(parts[1]);
+            }
+            match = match.NextMatch();
+        }
+        return total;
+    }
+
+    public static int Multiply(string input)
+    {
+        Regex mulPattern = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)");
+        Match match = mulPattern.Match(input);
+        int total = 0;
+        while (match.Success)
+        {
+            var parts = match.Value.Split(
+                new char[] { 'm', 'u', 'l', ',', '(', ')' },
+                StringSplitOptions.RemoveEmptyEntries
+            );
+            total += int.Parse(parts[0]) * int.Parse(parts[1]);
+            match = match.NextMatch();
+        }
+        return total;
+    }
 }
+
+
